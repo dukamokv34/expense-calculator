ad6e068d93fddaebddb77fd14c12770ca04abca5
dukamokv34 <duka.mo.educ@gmail.com>
2025-12-26 22:16:41 +0200

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,1 +1,18 @@
+from dataclasses import dataclass
+from datetime import date
 
+@dataclass(frozen=True)
+class Category:
+    name: str
+
+@dataclass
+class Expense:
+    amount: float
+    date: date
+    category: Category
+
+    def get_amount(self) -> float:
+        return self.amount
+
+    def get_category(self) -> Category:
+        return self.category
